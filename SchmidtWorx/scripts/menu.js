3e251178b6fda4cbdc8aa7106f6dac0e0cd4a1b4
schmidtistic <162704409+schmidtistic@users.noreply.github.com>
2026-02-28 20:45:17 -0500

    Nav troubleshooting

--- SchmidtWorx/scripts/menu.js
+++ SchmidtWorx/scripts/menu.js
@@ -1,6 +1,6 @@
 function toggleMenu() {
     const menu = document.getElementById("menu");
     if (menu) {
-        menu.classList.toggle("active");
+        menu.classList.toggle("open");
     }
 }
